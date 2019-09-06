85fb1872dbc329ce3125ab598333485b18644e53
fangyuandinggithub <3026354242@qq.com>
2019-09-06 11:19:57 +0800

    Initial Commit

---
+++ UploadToGithubTests/UploadToGithubTests.m
@@ -0,0 +1,37 @@
+//
+//  UploadToGithubTests.m
+//  UploadToGithubTests
+//
+//  Created by fangyuan ding on 2019/9/6.
+//  Copyright © 2019年 fangyuan ding. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface UploadToGithubTests : XCTestCase
+
+@end
+
+@implementation UploadToGithubTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
